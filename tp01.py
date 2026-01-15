a90e1e9366a8d06999548b563079018b00855ec2
ilyassKN <kninahilyass@gmail.com>
2026-01-15 09:33:15 +0100

    ilyass

--- tp01.py
+++ tp01.py
@@ -1,1 +1,7 @@
 # Créer une fonction qui renvoie le texte en majuscule
+
+def mettre_en_majuscule(texte):
+    texte =input("Entrez un texte : ")
+    return texte.upper()
+
+print(mettre_en_majuscule(""))  # Appel de la fonction et affichage du résultat
